74e4c716b5002ffc4ea3f79795a50b79aecf34bb
JNKarif <arif.rahman041@gmail.com>
2023-01-14 13:32:37 +0600

    banner-padding-height

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero h-auto bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt='' />
-          <div>
+          <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg  shadow-2xl" alt='' />
+          <div className='lg:p-20'>
             <h1 className="text-5xl font-bold">Onsite, Remote & hybrid jobs!</h1>
             <p className="py-6">We have 100s of factories, IT companies, Corporate Companies in our contact for your dream job</p>
             <button className="btn btn-primary">Get Started</button>
